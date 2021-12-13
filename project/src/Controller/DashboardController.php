30797d742dc5097a835df5377f1b23eda3dcc67a
BaptistePDJ <babou01250@gmail.com>
2021-12-13 09:41:41 +0100

    add some view

--- project/src/Controller/DashboardController.php
+++ project/src/Controller/DashboardController.php
@@ -1,6 +1,20 @@
 <?php
 
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
 class DashboardController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
+    #[Route('/', name: 'home')]
+    public function indexAction()
+    {
+        return $this->render('dashboard/index.html.twig');
+    }
 
+    #[Route('/liste', name: 'client-list')]
+    public function listAction()
+    {
+        return $this->render('dashboard/client-list.html.twig');
+    }
 }
