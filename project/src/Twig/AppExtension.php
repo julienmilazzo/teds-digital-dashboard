f4cdc77c1ee29db12d436f0e08da01c14fcefa56
BaptistePDJ <babou01250@gmail.com>
2021-12-21 09:44:18 +0100

    fix twig funtion

--- project/src/Twig/AppExtension.php
+++ project/src/Twig/AppExtension.php
@@ -2,33 +2,11 @@
 
 namespace App\Twig;
 
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
-    /**
-     * @var UrlGeneratorInterface
-     */
-    private $generator;
-
-    /**
-     * @var RequestStack
-     */
-    private $requestStack;
-
-    /**
-     * @param UrlGeneratorInterface $generator
-     * @param RequestStack $requestStack
-     */
-    public function __construct(UrlGeneratorInterface $generator, RequestStack $requestStack)
-    {
-        $this->generator = $generator;
-        $this->requestStack = $requestStack;
-    }
-
     /**
      * {@inheritDoc}
      */
